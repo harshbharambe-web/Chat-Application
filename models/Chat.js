4b94655ea926fb8b09dd07343a09656319a6edad
Harsh Bharambe <harshbharambe6@gmail.com>
2026-04-10 22:20:20 +0530

    Add Chat model with schema for messages

---
+++ models/Chat.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema(
+
+{
+
+    from : {
+        type : String,
+        required : true,
+
+    },
+
+    to : {
+        type : String,
+         required : true,
+    },
+
+    msg : {
+        type : String,
+
+    },
+
+    createAt : {
+        type : Date,
+         required : true,
+    }
+}
+
+
+);
+
+const chat = mongoose.model("chat", chatSchema);
+module.exports = chat;
