234f71c0e4ba6cbb8e43494722e173962f67d398
Harsh Bharambe <harshbharambe6@gmail.com>
2026-04-12 08:09:28 +0530

    Refactor chat application routes and methods

--- index.js
+++ index.js
@@ -1,125 +1,93 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const chat = require("./models/chat.js")
+const chat = require("./models/chat.js");
 const methodOverride = require("method-override");
 
-app.use(methodOverride("_method")); 
-
-
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname, "public")));
 
+// 1. Parse body FIRST so req.body is available for methodOverride
+app.use(express.urlencoded({ extended: true }));
 
+// 2. Now methodOverride can read _method from the parsed body
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === "object" && "_method" in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+app.use(methodOverride("_method")); // fallback: ?_method=DELETE in query string
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 main()
-
-.then(()=>{
-
-    console.log("Connection Sucessfull");
-    
-})
-
-
-
-
-.catch(err => console.log(err));
+  .then(() => { console.log("Connection Successful"); })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/chat');
-
+  await mongoose.connect("mongodb://127.0.0.1:27017/chat");
 }
 
+// HOME PAGE
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.get("/", (req,res) =>{
+// ALL CHATS
+app.get("/chats", async (req, res) => {
+  let chats = await chat.find().sort({ createAt: -1 });
+  res.render("index.ejs", { chats });
+});
 
-res.send("Welcome");
+// NEW CHAT FORM
+app.get("/chats/new", (req, res) => {
+  res.render("newChat.ejs");
+});
 
-})
-
-//home 
-
-app.get("/chats", async (req,res)=>{
-
-    let chats = await chat.find();
-
-    console.log(chats);
-    res.render("index.ejs", {chats});
-
-
-
-})
-
-//new chat
-
-app.get("/chats/new", (req,res)=>{
-    
-    res.render("newChat.ejs");
-
-})
-
-app.post("/chats",async (req,res)=>{
-
-    let {from,to,msg} = req.body;
-    let newChat = new chat({
-    from : from,
-    to : to,
+// CREATE CHAT
+app.post("/chats", async (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new chat({
+    from: from,
+    to: to,
     msg: msg,
-    createAt : new Date()});
+    createAt: new Date(),
+  });
+  await newChat.save();
+  res.redirect("/chats");
+});
 
-    newChat.save().then((res)=>{
-
-        console.log("Chat was saved");
-        
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-    res.redirect("/chats");
-    
-
-})
-
-
-//edit
+// EDIT FORM
 app.get("/chats/:id/edit", async (req, res) => {
-    let { id } = req.params;
-
-    let foundChat = await chat.findById(id);
-
-    res.render("edit.ejs", { chat: foundChat }); 
+  let { id } = req.params;
+  let foundChat = await chat.findById(id);
+  res.render("edit.ejs", { chat: foundChat });
 });
 
-//update
+// UPDATE CHAT
 app.put("/chats/:id", async (req, res) => {
-
-    let { id } = req.params;
-    let { msg } = req.body;
-
-    let updatedChat = await chat.findByIdAndUpdate(
-        id,
-        { msg },
-        { runValidators: true, new: true }
-    );
-
-    console.log(updatedChat);
-
-    res.redirect("/chats");
+  let { id } = req.params;
+  let { msg } = req.body;
+  let updatedChat = await chat.findByIdAndUpdate(
+    id,
+    { msg, updatedAt: new Date() },
+    { runValidators: true, new: true }
+  );
+  console.log("Updated:", updatedChat);
+  res.redirect("/chats?saved=1");
 });
 
-
+// DELETE CHAT
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  await chat.findByIdAndDelete(id);
+  console.log("Deleted chat:", id);
+  res.redirect("/chats?deleted=1");
+});
 
 app.listen(8080, () => {
-
-console.log("Server listening to 8080");
-
-
-
-})
-
-
-
+  console.log("Server listening on port 8080");
+});
